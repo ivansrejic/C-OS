ed118c73a63306dbbdd4a4bed41f88bf447df8b2
IvaTeca <ivateca@IvaTecas-MacBook-Pro.local>
2022-08-27 16:51:26 +0200

    Opste

--- OpsteNesto.c
+++ OpsteNesto.c
@@ -1,3 +1,6 @@
 int main(int argc,char* argv[]); // U C, svaka main funkcija treba da ima ovakvu strukturu. Prvi argument pokazuje koliko ustvari elemenata ima ovaj argv[]
 
 // Prvi argument programa je uvek sopstveno ime odnosno putanja do same sebe - argv[0] bi bio ./OpsteNesto koje je kompajlirano 
+
+double atof(char* str) -> Converts the string argument to a floating point number (double)
+int atoi(char* str) -> Converts the string to a int number 
