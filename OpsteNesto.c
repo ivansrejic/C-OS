d0129e54527b64e7debddf7b5c374de582674449
IvaTeca <ivateca@IvaTecas-MacBook-Pro.local>
2022-08-27 17:27:59 +0200

    aaa

--- OpsteNesto.c
+++ OpsteNesto.c
@@ -4,3 +4,4 @@ int main(int argc,char* argv[]); // U C, svaka main funkcija treba da ima ovakvu
 
 double atof(char* str) -> Converts the string argument to a floating point number (double)
 int atoi(char* str) -> Converts the string to a int number 
+itoa(i,str,10) -> Konvertuje int u string i osnova je 10 
